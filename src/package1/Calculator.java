c11392db5bf119e9fd85afa50b6a9a036bbbe06b
sg0017473 <sg0017473@INCDSZLP00147.TechMahindra.com>
2020-01-16 09:30:01 +0530

    FirstCommit

---
+++ src/package1/Calculator.java
@@ -0,0 +1,21 @@
+package package1;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Calculator  extends HttpServlet{
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int  i =Integer.parseInt(req.getParameter("num1"));
+		int  j= Integer.parseInt(req.getParameter("num2"));
+		
+		res.getWriter().println(i+j);
+		
+		
+	}
+
+}
